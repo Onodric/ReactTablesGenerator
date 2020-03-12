33c158a4a846143f8826036d8f838f457b1e70c8
Belve Marks <belvedmarks4@gmail.com>
2020-03-11 23:51:08 -0500

    Updates

---
+++ precache-manifest.aaa2e57d493f8cd7f887f681d6d1b2b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a7e794d376bb5bc15b2dc8e52b20e9",
+    "url": "/ReactTablesGenerator/index.html"
+  },
+  {
+    "revision": "e4b9030146151f14a514",
+    "url": "/ReactTablesGenerator/static/css/main.9b689d91.chunk.css"
+  },
+  {
+    "revision": "8c1dc54f3320846906c0",
+    "url": "/ReactTablesGenerator/static/js/2.87c9b25e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ReactTablesGenerator/static/js/2.87c9b25e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4b9030146151f14a514",
+    "url": "/ReactTablesGenerator/static/js/main.fb229d80.chunk.js"
+  },
+  {
+    "revision": "58e489224bc3a79109e5",
+    "url": "/ReactTablesGenerator/static/js/runtime-main.a8be3ae0.js"
+  }
+]);
